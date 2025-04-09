76d893243573192f22791dc1f447c1dfd31d04a8
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2025-04-10 03:44:38 +0800

    Publish Package to npmjs

---
+++ build_npm.ts
@@ -0,0 +1,37 @@
+import { build, emptyDir } from '@deno/dnt';
+import denoJson from './deno.json' with { type: 'json' };
+
+await emptyDir('./npm');
+
+await build({
+  entryPoints: ['./hello-world.ts', './hello-world.json'],
+  outDir: './npm',
+  skipNpmInstall: true,
+  test: false,
+  compilerOptions: {
+    lib: ['ESNext'],
+  },
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: '@t1ckbase/jsr-hello-world',
+    version: denoJson.version,
+    description: 'jsr-hello-world',
+    license: 'MIT',
+    repository: {
+      type: 'git',
+      url: 'git+https://github.com/T1ckbase/jsr-hello-world.git',
+    },
+    bugs: {
+      url: 'https://github.com/T1ckbase/jsr-hello-world/issues',
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync('LICENSE', 'npm/LICENSE');
+    Deno.copyFileSync('README.md', 'npm/README.md');
+  },
+});
